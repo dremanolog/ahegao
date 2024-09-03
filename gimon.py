dab96d455a393e9c4e34f66750a39a5cd51fbf18
dremanolog <148541907+dremanolog@users.noreply.github.com>
2024-09-03 12:48:11 +0300

    Update gimon.py

---
+++ gimon.py
@@ -0,0 +1,22 @@
+def sum(x,y):
+    result=a+b
+    return sum
+def min(x,y):
+    min=a-b
+    return min
+def delenie(x,y):
+    result=a/b
+    return delenie
+def umn(x,y):
+    result=a*b
+    return umn
+print("этот калькулятор умножает, складывает, делит и вычитает")
+print("для выбора действия впишите + - / *")
+a=int(input("первое число"))
+c=str(input("введите действие"))
+b=int(input("второе число"))
+l=[]
+if c=="+":
+    print(sum)
+else:
+    print(0)
